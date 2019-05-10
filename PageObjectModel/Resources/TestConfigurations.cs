3f18b5eceb736f3d11e68a0dfd5211df06df4373
Sai Kiran <saikiran40cs@users.noreply.github.com>
2019-05-10 19:14:35 +0530

    Create TestConfigurations.cs

---
+++ PageObjectModel/Resources/TestConfigurations.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTestFramework.Resources
+{
+    public static class TestConfigurations
+    {
+        //Browser to Launch
+        public static string browserName = "";
+        //URL's to launch in the browser       
+        public static readonly string baseUrl = "https://www.google.com/";
+    }
+    
+}
